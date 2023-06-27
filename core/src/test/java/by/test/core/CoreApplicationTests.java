4de27082e92400a4cf91e32706980c03ae4cda64
Kingl <siarheitsarik@gmail.com>
2023-06-27 22:24:51 +0300

    Создана структура проекта.

---
+++ core/src/test/java/by/test/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package by.test.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
